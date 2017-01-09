05fedf21c2420931553fbec35731f4c1e9b2ca17
William <willi4m@zju.edu.cn>
2017-01-09 15:26:03 +0800

    [A] class Cell & class FakeCell

---
+++ +fs/+targets/+cells/@Cell/Cell.m
@@ -0,0 +1,14 @@
+classdef Cell < fs.targets.Target
+    %CELL contains 
+    %   ...
+    
+    %% Public Properties
+    properties (Access = public)
+        %
+    end
+    %% 
+    methods
+    end
+    
+end
+
---
+++ +fs/+targets/+cells/@FakeCell/FakeCell.m
@@ -0,0 +1,12 @@
+classdef FakeCell < fs.targets.cells.Cell
+    %FAKECELL ...
+    %   ...
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
