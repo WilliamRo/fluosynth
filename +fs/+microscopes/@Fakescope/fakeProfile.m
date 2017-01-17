e61ce8637161f3239a345cc09e7d137f33f66f0d
William <willi4m@zju.edu.cn>
2017-01-17 16:17:43 +0800

    [M] fakescope::fakeprofile

--- +fs/+microscopes/@Fakescope/fakeProfile.m
+++ +fs/+microscopes/@Fakescope/fakeProfile.m
@@ -9,8 +9,8 @@ assert(bound > 0, '!! Input bound is illegal')
 
 % generate curve
 d = 0 : bound;
-% y = exp(-d / omega);
-y = normpdf(d, 0, sigma);
+y = exp(-d.^2 / (2*sigma*sigma));
+% y = normpdf(d, 0, sigma);
 y = y / max(y);
 
 end
